7e93f8e58404c39b6a5fed847b8184150a70c6b1
Winslow <sobhanyazdanjo@gmail.com>
2021-01-16 08:11:34 +0330

    completed

--- passwordPractice/app.js
+++ passwordPractice/app.js
@@ -1,4 +1,11 @@
 // Write a isValidPassword function
+function isValidPassword(password, username){
+    if ((password.length >= 8) && (password.includes(" ") === false) && 
+    (password.includes(username)) === false) {
+        return true;
+    }
+    return false;   
+}
 // It accepts 2 arguments: password and username
 // Password must:
 //	- be at least 8 characters
@@ -7,6 +14,8 @@
 // If all requirements are met, return true.
 //Otherwise: false
 
-// isValidPassword('89Fjj1nms', 'dogLuvr');  //true
-// isValidPassword('dogLuvr123!', 'dogLuvr') //false
-// isValidPassword('hello1', 'dogLuvr') //false
+let pass1 = isValidPassword('89Fjj1nms', 'dogLuvr');  //true
+let pass2 = isValidPassword('dogLuvr123!', 'dogLuvr') //false
+let pass3 = isValidPassword('hello1', 'dogLuvr') //false
+
+console.log(`pass1 is ${pass1}\npass2 is ${pass2}\npass3 is ${pass3}`);
